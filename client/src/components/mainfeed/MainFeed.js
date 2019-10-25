fd6fef47dd019acfdcbc04b8430e9b27437e2f2f
Thomas Foydel <tfoydel@mail.roosevelt.edu>
2019-10-25 18:28:15 -0500

    21

--- client/src/components/mainfeed/MainFeed.js
+++ client/src/components/mainfeed/MainFeed.js
@@ -10,6 +10,7 @@ import { setPosts } from '../../redux/posts/postActions';
 import './MainFeed.scss';
 
 const MainFeed = ({ setReduxPosts, reduxPosts, token, isLoggedIn }) => {
+  console.log('is logged in: ', isLoggedIn);
   const [postFormOpen, setPostFormOpen] = useState(false);
   const [count, setCount] = useState(5);
   const [start, setStart] = useState(1);
