a0b484cea5c470c2c9fe20f22b3e9517d47c4717
Thomas Foydel <tfoydel@mail.roosevelt.edu>
2019-12-02 23:58:58 -0600

    fixed friendrequest reject

--- client/src/pages/editUser/EditUser.js
+++ client/src/pages/editUser/EditUser.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { connect } from 'react-redux';
-import { setCurrentUserInfo } from '../../redux/currentUser/currentUserActions';
-import UploadProfilePic from '../../components/uploadprofilepic/UploadProfilePic';
-import UploadCoverPic from '../../components/uploadcoverpic/UploadCoverPic';
+import { setCurrentUserInfo } from '../../../redux/currentUser/currentUserActions';
+import UploadProfilePic from '../../../components/uploadprofilepic/UploadProfilePic';
+import UploadCoverPic from '../../../components/uploadcoverpic/UploadCoverPic';
 
 import './EditUser.scss';
 
