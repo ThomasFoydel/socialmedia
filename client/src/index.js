c9ff26da846fa40e4709a225157e5495f4dc22e7
Thomas Foydel <tfoydel@mail.roosevelt.edu>
2019-12-20 22:25:00 -0600

    fixed feed post comment section issue

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
-
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 
--- client/src/pages/feed/mainfeed/feedpost/FeedPostCommentSection.js
+++ client/src/pages/feed/mainfeed/feedpost/FeedPostCommentSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Comment from './commentform/CommentForm';
+import Comment from './comment/Comment';
 import { useTransition, animated, config } from 'react-spring';
 
 const FeedPostCommentSection = ({
